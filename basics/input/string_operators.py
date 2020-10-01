dd76b31597ec8df39d026388e62217397a7fd6b9
repl.it user <replituser@example.com>
2020-10-01 19:28:31 +0000

    Created string operators

---
+++ basics/input/string_operators.py
@@ -0,0 +1,9 @@
+lives = int (input  ("Please enter the number of lives "))
+print ("Bleep has ", lives, " lives")
+energy = int (input  ("Please enter the energy level "))
+print ("Bleep has ", energy, " energy")
+shields = int (input  ("Please enter the shield level "))
+print ("Showing display")
+print ("")
+print ("Lives: ", lives * "♥")
+
