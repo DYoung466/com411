7b97c89eb31f1fcd63a20667712ce947bc7e6e07
repl.it user <replituser@example.com>
2020-10-01 19:58:02 +0000

    Updated review.py

--- basics/input/review.py
+++ basics/input/review.py
@@ -2,3 +2,5 @@ print ("Hi, I'm Bleep")
 nameVar = input ("What's your name?")
 print ("Hi, ",nameVar)
 helpVar = input ("My energy levels are low, could you help?")
+if (helpVar == "Yes") or (helpVar == "Yeah"):
+  print ("yes")
