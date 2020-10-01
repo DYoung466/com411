8e6d54a968fd4263a208d75c71982ea49c9f30c9
repl.it user <replituser@example.com>
2020-10-01 20:21:06 +0000

    Updated review.py

--- basics/input/review.py
+++ basics/input/review.py
@@ -1,6 +1,24 @@
 print ("Hi, I'm Bleep")
-nameVar = input ("What's your name?")
-print ("Hi, ",nameVar)
-helpVar = input ("My energy levels are low, could you help?")
-if (helpVar == "Yes") or (helpVar == "Yeah"):
-  print ("yes")
+nameVar = input ("What's your name? ")
+print ("Hi,",nameVar)
+print ("")
+helpVar = input ("My energy levels are low, could you help? ")
+if (helpVar == "Yes") or (helpVar == "Yeah") or (helpVar == "yes") or (helpVar == "yeah"):
+  print ("Thanks")
+else:
+  print("Oh well, thanks anyway")
+print ("")
+print ("Please solve these programming questions to build up my energy!")
+print ("Which of these shows the user text?")
+print ("")
+print ("A. print")
+print ("B. Text")
+print ("C. primt")
+print ("D. beans")
+A1Var = input ("Please type the letter of your answer")
+if (A1Var == "A") or (A1Var == "a"):
+  print("Correct!") 
+elif (A1Var =="print") or (A1Var =="Print"):
+  print("Correct, but please only enter the letter")
+else:
+  print("Incorrect, the corr")
