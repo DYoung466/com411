b127e57e003c5041711b7d31799eb66b10ff728a
repl.it user <replituser@example.com>
2020-10-07 11:49:18 +0000

    Added code to demonstrate counting down with a for loop.

---
+++ basics/repetitions/for_loops/count_down.py
@@ -0,0 +1,8 @@
+print("I see a cave!")
+distance = int (input ("How many steps away is the cave? "))
+print ("The cave is ", distance, " steps away")
+steps = (distance)
+for distance in range (distance):
+  print (steps, " steps remaining")
+  steps = (steps - 1)
+print ("We're at the cave!")
