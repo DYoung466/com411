2e13126f2e68c5c700aadf584f484bd64b866970
repl.it user <replituser@example.com>
2020-10-07 14:50:44 +0000

    Started  nested loops

---
+++ basics/repetitions/nested_loop/nested.py
@@ -0,0 +1,10 @@
+row_var = int(input ("How many rows should there be? "))
+collumn_var = int(input ("How many collumns should there be? "))
+print ("Coming right up")
+for collumn_var in range(collumn_var) :
+  for row_var in range(row_var) :
+    print (":)",)
+print ("Done!")  
+
+#This is not finished yet and does not provide the right outcome
+#Needs to be finished
