f189c5039190ca86ca6fbc85d190a51891bd9564
repl.it user <replituser@example.com>
2020-10-07 11:37:49 +0000

    New File

---
+++ basics/repetitions/for_loops/simple.py
@@ -0,0 +1,11 @@
+print ("Mountains!")
+count = int( input ("How many mountains would you like? "))
+print (count, " mountains coming up!")#
+for count in range (count):
+  print ("           __")
+  print ("          /  \_  ")
+  print ("         /^    \ ")
+  print ("        /  ^    \_")
+  print ("      _/ ^ ^     ^\ ")
+  print ("     /  ^     ^    \ ")
+  print (" ---------------------------")
