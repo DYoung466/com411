05e64a965ee290144fef9b6a5f870705b9fa48e3
repl.it user <replituser@example.com>
2020-10-09 11:58:15 +0000

    Updated commenting on nested.py

--- basics/repetitions/nested_loop/nested.py
+++ basics/repetitions/nested_loop/nested.py
@@ -1,10 +1,15 @@
+
 row_var = int(input ("How many rows should there be? "))
 collumn_var = int(input ("How many collumns should there be? "))
+
+
 print ("Coming right up")
-for collumn_var in range(collumn_var) :
-  for row_var in range(row_var) :
-    print (":)",)
+for row_var in range(0, row_var, 1) :
+  for collumn_var in range(0, collumn_var, 1) :
+      print (":)", end="")
+  print("")
 print ("Done!")  
 
-#This is not finished yet and does not provide the right outcome
-#Needs to be finished
+#The solution for this question has to  be wrong? 
+#The collumn var always decreases as  it iterates through the loop 
+#I can't think of a way to do this without introducing more variable and  this isn't in the question
