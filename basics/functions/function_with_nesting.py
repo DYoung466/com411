2e940e3df55b2c87ffeb76fed1442cf51f0b7af1
repl.it user <replituser@example.com>
2020-10-13 10:10:07 +0000

    Added code to demomnstrate the use of a user defined function with nesting

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,9 @@
+#Define the function identify
+def identify ():
+  incoming_input = input ("What do you see? ")
+  if (incoming_input == ("a large boulder")):
+    print("It's time to run!")
+  else:
+    print ("We will probaly be fine")
+#calls the function identify
+identify () 
