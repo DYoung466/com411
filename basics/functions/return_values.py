198abe6a4c0c1e0524f2c90264d98261cd93459c
repl.it user <replituser@example.com>
2020-10-14 14:19:57 +0000

    Backing up changes to return_values.py for git pull

--- basics/functions/return_values.py
+++ basics/functions/return_values.py
@@ -1,29 +1,31 @@
 #Defines the functions:
 
 def sum_weights(beep_weight,bop_weight):
-  total_weight = float(( beep_weight + bop_weight))
-  return total_weight
+  total_weight = (( beep_weight + bop_weight))
+  return  total_weight
 
-def calc_avg_weights (beep_weight_bop_weight):
-  avg_weight = float((sum_weights/2))
-  return avg_weight
+def calc_avg_weights (beep_weight,bop_weight):
+  avg_weight = float(((beep_weight+bop_weight)/2))
+  return float( avg_weight)
 
 def run ():
   user_choice = input ("Sum or average? ")
   if (user_choice == "sum") :
     print("Sum!")
     print("")
-    beep_weight = input ("Please entert Beep's weight ")
-    bop_weight = input ("Please entert Bop's weight ")
-    sum_weights (beep_weight,bop_weight)
-    print (sum_weights)
+    beep_weight = int(input ("Please enter Beep's weight "))
+    bop_weight = int(input ("Please enter Bop's weight "))
+    totalSum = sum_weights(beep_weight,bop_weight)
+    print(totalSum)
+  
   elif (user_choice == "average"):
     print("average")
     print("")
-    beep_weight = input ("Please entert Beep's weight ")
-    bop_weight = input ("Please entert Bop's weight ")
-    calc_avg_weights (beep_weight,bop_weight)
-    print (calc_avg_weights)
+    beep_weight = float(input ("Please enter Beep's weight "))
+    bop_weight = float(input ("Please enter Bop's weight "))
+    totalAvg = calc_avg_weights (beep_weight,bop_weight)
+    print (totalAvg)
+
 
 run()
 
