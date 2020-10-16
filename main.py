7b352f81ecda294e3e463122b5631dcd5302af3d
repl.it user <replituser@example.com>
2020-10-16 15:45:26 +0000

    started additions to main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import basics.output.simple_message as simple_message
+import basics.output.multiline_message as multiline_message
+import basics.output.escape_characters as escape_message
+import basics.output.ascii_art.py as ascii_art
+import basics.input.user_input as simple_input
+import basics.input.string_operators as string_operators
+import basics.input.review as review
+import basics.input.data_types as data_types
+import basics.input.ascii_robot as ascii_robot
+import basics.decisions.nested_decisions.nestception as nestception
+import basics.decisions.simple_decisions.modulo_operator as modulo_operator
+import basics.decisions.simple_decisions.if as decisions_if
+import basics.decisions.simple_decisions.if_else as if_else
+import basics.decisions.simple_decisions.if_elif_else as if_elif_else
+import basics.decisions.simple_decisions.counter as counter
+import basics.decisions.simple_decisions.comparison_operators as comparison_operators
+import basics.repetitions.for_loops.characters as characters
+import basics.repetitions.for_loops.count_down as count_down
+import basics.repetitions.for_loops.membership_operators as membership_operators
+import basics.repetitions.for_loops.range as for_range
+import basics.repetitions.for_loops.reverse as for_reverse
+import basics.repetitions.for_loops.simple as simple
+import basics.repetitions.nested_loops.nested as nested_loops
+import basics.repetitions.while_loops.factorial as factorial
+
+
+
+
+
+
+
+
+
+
+
+
+
+
