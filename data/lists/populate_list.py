be64fcf0b05bd0fc91fad7b7358c3ef32aa8bb96
repl.it user <replituser@example.com>
2020-10-20 11:04:20 +0000

    Started pupulate_list.py

--- data/lists/populate_list.py
+++ data/lists/populate_list.py
@@ -7,6 +7,20 @@ def menu ():
   directions_var = directions()
   for index in range(0,len(directions_var),1):
     print("{} : {}".format (index,directions_var[index]))
+    
 
+def run():
+  route = []
+  directions_var = directions()
+  print("Working out escape route")
+  for index in range (0,3,1):
+    menu_var = menu()
+    route.append(input("Choose a direction "))
+  route_var = route
+  print (directions_var[route_var])
 
-menu()
+  
+  
+ 
+
+run()
