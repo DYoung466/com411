909755f6259dc19e792f35ae456b5b1b57a61905
repl.it user <replituser@example.com>
2020-10-20 11:40:06 +0000

    Commiting before merge

--- data/lists/populate_list.py
+++ data/lists/populate_list.py
@@ -7,6 +7,21 @@ def menu ():
   directions_var = directions()
   for index in range(0,len(directions_var),1):
     print("{} : {}".format (index,directions_var[index]))
+  direction_index  = int(input ("Choose a direction "))
+  return directions_var[direction_index]
 
+def run():
+  route = []
+  directions_var = directions()
+  print("Working out escape route")
+  for index in range (0,5,1):
+    menu_var = menu()
+    route.append(menu_var)
+  print(route)
+  
 
-menu()
+  
+  
+ 
+
+run()
