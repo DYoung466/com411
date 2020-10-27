67fda7cd5fc886a439bc28a3cc6aeab1fcad2443
repl.it user <replituser@example.com>
2020-10-27 10:56:19 +0000

    Added code to demonstrate a simple set

--- data/tuples/nested_tuples.py
+++ data/tuples/nested_tuples.py
@@ -1,10 +1,10 @@
 def steps ():
   likelihoods = []
-  likelihoods.append (("Step1", 50))
-  likelihoods.append (("Step2", 38))
-  likelihoods.append (("Step3", 27))
-  likelihoods.append (("Step4", 99))
-  likelihoods.append (("Step5", 4))
+  likelihoods.append (("Step 1", 50))
+  likelihoods.append (("Step 2", 38))
+  likelihoods.append (("Step 3", 27))
+  likelihoods.append (("Step 4", 99))
+  likelihoods.append (("Step 5", 4))
   return likelihoods
 
 def run():
@@ -12,13 +12,14 @@ def run():
   likelihoods_var = steps()
   good_steps = []
   bad_steps = []
+
   for likelihood in likelihoods_var:
     if (likelihoods_var[1] >= 50):
       bad_steps.append (likelihood)
     else:
       good_steps.append (likelihood)
       
-  print ("There are ",len(good_steps))
-  print ("There are ",len(bad_steps))
+  print ("There are ",len(good_steps), "good steps")
+  print ("There are ",len(bad_steps), "bad steps")
 
 run()
