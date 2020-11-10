1ccfcedc9b86c0176bb4c3983eea8fd9595f3d06
repl.it user <replituser@example.com>
2020-11-10 10:04:30 +0000

    Comitting before pull

--- data/files/txt/write.py
+++ data/files/txt/write.py
@@ -5,16 +5,31 @@ def search (filePath):
   with open(filePath) as fileVar:
     for lines in fileVar:
       if lines.startswith("Section"):
-        splitVar = lines.split(",")
-        sections.append(splitVar)
+        splitVar = lines.split(":")
+        sections.append(splitVar[1].replace ('\n',""))
       else:
         books.append(lines)
     print (sections,books)
+  print("Done")
   return (sections,books)
+  
+def save(fileName,data):
+  print("Saving")
+  with open (fileName,"w") as file:
+    file.write("Sections: ")
+    for section in data [0]:
+      file.write (f"{section},")
+
+    file.write("\nBooks ")
+    for book in data [1]:
+      file.write (f"{book},")
+  print("Done")
+
+  
 
 def run():
-  search("data/files/txt/books.txt")
+  data = search("data/files/txt/books.txt")
+  save ("data/files/txt/sections-books.txt",data)
 
-
-        
 run()
+
