419914edb3356b502fb19ebb316efe76d87c1210
repl.it user <replituser@example.com>
2020-11-10 12:15:07 +0000

    Started dict.py

---
+++ visual/plots/dict.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt 
+
+def data():
+  paths = {}
+  line_type = input ("What type of line would you like? ")
+  line_colour = input ("What colour line would you like? ")
+  line_marker = input ("What marker would you like? ")
+  paths["ltype"] = line_type
+  paths["colour"] = line_colour
+  paths["marker"] = line_marker
+  return paths
+
+def generate():
+  x_list = [1,2,3]
+  y_list = [1,2,3]
+  line_amount = int (input("How many lines? "))
+  for i in range(line_amount):
+    data_var = data()
+    print (data_var)
+    data_combined = ["colour"]+["marker"]+["ltype"]
+    plt.xlabel("x values")
+    plt.ylabel("y values")
+    plt.plot(x_list, y_list, data_combined)
+  plt.show
+
+def run():
+  print("Running...")
+  generate()
+  print("Done")
+
+run()
