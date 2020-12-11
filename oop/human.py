48acf27b6ef9e2e417a78cf9b3df7b5c2e693037
Daniel Young <5yound67@solent.ac.uk>
2020-12-11 15:45:15 +0000

    Updated human and robot with repr method

--- oop/human.py
+++ oop/human.py
@@ -9,4 +9,10 @@ class human:
   def display(self):
     print(f"I am {self.name}")
 
+  def __str__(self):
+    return f"{self.name} is {self.age} years old"
+
+  def __repr__(self):
+    return f"human(name={self.name}, age = {self.age})"
+
 
--- oop/robot.py
+++ oop/robot.py
@@ -12,3 +12,8 @@ class robot:
     #method
     print(f"I am {self.name}")
 
+  def __str__(self):
+    return f"{self.name} is {self.age} years old"
+
+  def __repr__(self):
+    return f"robot(name={self.name}, age = {self.age})"
